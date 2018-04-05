3dec8ccdb8fea382577e96fc3e9dde5394ccf061
Alex Galinier <contact@alexgalinier.com>
2018-04-05 10:54:53 +0200

    feat(font-size): Adapt text size to parent height

--- src/app/budget/graph/Graph.js
+++ src/app/budget/graph/Graph.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Block } from 'a_design-components';
 import { BudgetService } from './../Budget.service';
 import Crosslines from './Crosslines';
 import Col from './Col';
 import './Graph.css';
 
-export default ({ budget }) => {
-  const catMax = BudgetService.maxBudget();
+export default class Graph extends Component {
+  state = {
+    height: null,
+  };
 
-  return (
-    <div className="graph">
-      <div id="info-over" style={{ display: 'none' }} />
-      <div className="graph-content">
-        <Block title="Bugdet">
-          <div className="legend">
-            <Crosslines max={catMax} />
-          </div>
-          <div className="columns">
-            {budget.map(_ => (
-              <Col
-                key={_.id}
-                label={_.label}
-                id={_.labelId}
-                color={_.color}
-                max={catMax}
-                data={_.group ? _.group : _.data}
-              />
-            ))}
-          </div>
-        </Block>
+  componentDidMount() {
+    this.setState({
+      height: document.getElementById('graph-columns').clientHeight,
+    });
+  }
+
+  render() {
+    const { budget } = this.props;
+    const catMax = BudgetService.maxBudget();
+
+    return (
+      <div className="graph">
+        <div id="info-over" style={{ display: 'none' }} />
+        <div className="graph-content">
+          <Block title="Bugdet">
+            <div className="legend">
+              <Crosslines max={catMax} />
+            </div>
+            <div className="columns" id="graph-columns">
+              {budget.map(_ => (
+                <Col
+                  key={_.id}
+                  label={_.label}
+                  id={_.labelId}
+                  color={_.color}
+                  max={catMax}
+                  parentHeight={this.state.height}
+                  data={_.group ? _.group : _.data}
+                />
+              ))}
+            </div>
+          </Block>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
